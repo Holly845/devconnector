7068eb9c2dbf37723a48646aedf4134761ea76e3
Holly845 <43096415+Holly845@users.noreply.github.com>
2018-09-09 12:00:26 +0100

    Create keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // mongoURI: "mongodb://ross:Dublin11@ds247001.mlab.com:47001/devconnector"
+  // mongoURI: "mongodb://<dbuser>:<dbpassword>@ds249992.mlab.com:49992/devconnector"
   mongoURI: "mongodb://127.0.0.1:27017/devconnector"
 };
